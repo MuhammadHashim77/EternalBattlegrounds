7b34b0969ee1e42aacb2e29939691709fce03a65
MuhammadHashim77 <hashimtariq.work@gmail.com>
2023-08-05 23:11:29 +0500

    improved firing

--- Assets/Scripts/CoreGameplay/Player/ProjectileLauncher.cs
+++ Assets/Scripts/CoreGameplay/Player/ProjectileLauncher.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:78872fc3f5439bbeb5c4587ef1b60f9ce297a0564cbc03fd48c809c97668baef
-size 2088
+oid sha256:98f6526bfb6e894c6175c06b79116e0c821e54dd4f6a00b81ea7ed26dd27ab41
+size 3327
