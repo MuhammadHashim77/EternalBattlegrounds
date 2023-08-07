2f7e1f758de4abda4fc257399c4088bd55b2e395
MuhammadHashim77 <hashimtariq.work@gmail.com>
2023-08-07 07:57:53 +0500

    Joining lobby logic

---
+++ Assets/Scripts/UI/LobbiesList.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:f779a28602b17bf97564712af7a3040ea82d305a34001408f56e45968f69fd1c
+size 2237
---
+++ Assets/Scripts/UI/LobbyItem.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:be599682d266bbe2b11cac456e3f9af72b987aee860a5a1568aa2f7f275f2d4f
+size 686
