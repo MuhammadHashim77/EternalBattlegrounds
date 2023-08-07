99370a1fc57e52c16cf4fce0256a04f66b516f31
MuhammadHashim77 <hashimtariq.work@gmail.com>
2023-08-07 06:56:18 +0500

    Client Joining

--- Assets/Scripts/Networking/Host/HostGameManager.cs
+++ Assets/Scripts/Networking/Host/HostGameManager.cs
@@ -45,7 +45,7 @@ public class HostGameManager
 
         UnityTransport transport = NetworkManager.Singleton.GetComponent<UnityTransport>();
 
-        RelayServerData relayServerData = new RelayServerData(allocation, "udp");
+        RelayServerData relayServerData = new RelayServerData(allocation, "dtls");
         transport.SetRelayServerData(relayServerData);
 
         NetworkManager.Singleton.StartHost();
