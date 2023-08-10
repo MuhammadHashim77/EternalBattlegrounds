9b43ee8ac723d4214d0b78365893c93a96054baa
MuhammadHashim77 <hashimtariq.work@gmail.com>
2023-08-10 15:47:52 +0500

    Added player name display

--- Assets/Scripts/CoreGameplay/Player/Player.cs
+++ Assets/Scripts/CoreGameplay/Player/Player.cs
@@ -3,18 +3,30 @@ using System.Collections.Generic;
 using Unity.Netcode;
 using UnityEngine;
 using Cinemachine;
+using Unity.Collections;
 
 public class Player : NetworkBehaviour
 {
     [Header("References")]
+
     [SerializeField] private CinemachineVirtualCamera cinemachineVirtualCamera;
 
 
     [Header("Settings")]
+
     [SerializeField] private int ownerPriority = 15;
+
+    public NetworkVariable<FixedString32Bytes> PlayerName = new NetworkVariable<FixedString32Bytes>();
 
     public override void OnNetworkSpawn()
     {
+        if (IsServer)
+        {
+            UserData userData = HostSingleton.Instance.GameManager.NetworkServer.GetUserDataByClientId(OwnerClientId);
+
+            PlayerName.Value = userData.userName;
+        }
+
         if (IsOwner)
         {
             cinemachineVirtualCamera.Priority = ownerPriority; 
--- Assets/Scripts/Networking/Host/HostGameManager.cs
+++ Assets/Scripts/Networking/Host/HostGameManager.cs
@@ -26,7 +26,7 @@ public class HostGameManager : IDisposable
 
     private string lobbyId;
 
-    private NetworkServer networkServer;
+    public NetworkServer NetworkServer { get; private set; }
 
     public async Task StartHostAsync()
     {
@@ -84,7 +84,7 @@ public class HostGameManager : IDisposable
             return;
         }
 
-        networkServer = new NetworkServer(NetworkManager.Singleton);
+        NetworkServer = new NetworkServer(NetworkManager.Singleton);
 
         UserData userData = new UserData
         {
@@ -130,6 +130,6 @@ public class HostGameManager : IDisposable
             lobbyId = string.Empty;
         }
 
-        networkServer?.Dispose();
+        NetworkServer?.Dispose();
     }
 }
