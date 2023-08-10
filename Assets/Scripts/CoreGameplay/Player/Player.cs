573a31f220ea16c963d7498d3b6ab32b2c8d79f0
MuhammadHashim77 <hashimtariq.work@gmail.com>
2023-08-10 16:56:38 +0500

    added player death

--- Assets/Scripts/CoreGameplay/Player/Player.cs
+++ Assets/Scripts/CoreGameplay/Player/Player.cs
@@ -4,12 +4,15 @@ using Unity.Netcode;
 using UnityEngine;
 using Cinemachine;
 using Unity.Collections;
+using System;
 
 public class Player : NetworkBehaviour
 {
     [Header("References")]
 
     [SerializeField] private CinemachineVirtualCamera cinemachineVirtualCamera;
+
+    [field: SerializeField] public Health Health { get; private set; }
 
 
     [Header("Settings")]
@@ -18,6 +21,9 @@ public class Player : NetworkBehaviour
 
     public NetworkVariable<FixedString32Bytes> PlayerName = new NetworkVariable<FixedString32Bytes>();
 
+    public static event Action<Player> OnPlayerSpawned;
+    public static event Action<Player> OnPlayerDespawned;
+
     public override void OnNetworkSpawn()
     {
         if (IsServer)
@@ -25,11 +31,21 @@ public class Player : NetworkBehaviour
             UserData userData = HostSingleton.Instance.GameManager.NetworkServer.GetUserDataByClientId(OwnerClientId);
 
             PlayerName.Value = userData.userName;
+
+            OnPlayerSpawned?.Invoke(this);
         }
 
         if (IsOwner)
         {
             cinemachineVirtualCamera.Priority = ownerPriority; 
+        }
+    }
+
+    public override void OnNetworkDespawn()
+    {
+        if (IsServer)
+        {
+            OnPlayerDespawned?.Invoke(this);
         }
     }
 }
