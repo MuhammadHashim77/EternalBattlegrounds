16352a417f9391c63ceb5dc8d3c2ad4b17994415
MuhammadHashim77 <hashimtariq.work@gmail.com>
2023-08-10 16:56:38 +0500

    added player death

---
+++ Assets/Scripts/CoreGameplay/Combat/DeathHandler.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:6a3f5a828ebcc898b65dfc414369ca03aeeede625b5a6b859967b2649cc254ae
+size 1158
--- Assets/Scripts/CoreGameplay/Player/Player.cs
+++ Assets/Scripts/CoreGameplay/Player/Player.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1fa8a03351db6f0ec82fe814df41fc0010a2efe8992fb615440ef0b0ece0b725
-size 854
+oid sha256:2bc8bf57dc36a681db90e400f6046a9a57f4047eaf7c8df9011677b3b2ebb9ea
+size 1243
