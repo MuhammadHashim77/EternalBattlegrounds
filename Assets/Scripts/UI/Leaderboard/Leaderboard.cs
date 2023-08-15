8da4d1544a220c12b434b04e3b1a507c373a95d3
MuhammadHashim77 <138707982+MuhammadHashim77@users.noreply.github.com>
2023-08-15 21:38:54 +0500

    Leaderboard ui

---
+++ Assets/Scripts/UI/Leaderboard/Leaderboard.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:b82bf49bd1da3c3b9472e48f0c8b858e0bc58d9d26db920a3e733ffbc914a8ea
+size 295
---
+++ Assets/Scripts/UI/Leaderboard/LeaderboardEntityDisplay.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:dd3d32d15394a4548885c696d676a26cde0524fcc5dfb8e665adbde21b38c69d
+size 308
