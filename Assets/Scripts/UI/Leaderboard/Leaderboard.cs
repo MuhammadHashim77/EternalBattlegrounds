47cb6f383af6242c7328f2d967ff9a67a17a25f2
MuhammadHashim77 <138707982+MuhammadHashim77@users.noreply.github.com>
2023-08-16 01:03:28 +0500

    leaderboard addition

--- Assets/Scripts/UI/Leaderboard/Leaderboard.cs
+++ Assets/Scripts/UI/Leaderboard/Leaderboard.cs
@@ -1,18 +1,97 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Netcode;
 using UnityEngine;
 
-public class Leaderboard : MonoBehaviour
+public class Leaderboard : NetworkBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private Transform leaderboardEntityHolder;
+
+    [SerializeField] private LeaderboardEntityDisplay LeaderboardEntityPrefab;
+
+    private NetworkList<LeaderboardEntityState> leaderboardEntities;
+
+    private void Awake()
     {
-        
+        leaderboardEntities = new NetworkList<LeaderboardEntityState>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public override void OnNetworkSpawn()
     {
-        
+        if (IsClient)
+        {
+            leaderboardEntities.OnListChanged += HandleLeaderboardEntitiesChanged;
+
+            foreach(LeaderboardEntityState entity in leaderboardEntities)
+            {
+                HandleLeaderboardEntitiesChanged(new NetworkListEvent<LeaderboardEntityState>
+                {
+                    Type = NetworkListEvent<LeaderboardEntityState>.EventType.Add,
+                    Value = entity
+                });
+            }
+        }
+
+        if (IsServer)
+        {
+            Player[] players = FindObjectsOfType<Player>();
+
+            foreach(Player player in players)
+            {
+                HandlePlayerSpawned(player);
+            }
+
+            Player.OnPlayerSpawned += HandlePlayerSpawned;
+            Player.OnPlayerDespawned += HandlePlayerDespawned;
+        }
+    }
+
+    private void HandleLeaderboardEntitiesChanged(NetworkListEvent<LeaderboardEntityState> changeEvent)
+    {
+        switch (changeEvent.Type)
+        {
+            case NetworkListEvent<LeaderboardEntityState>.EventType.Add:
+                Instantiate(LeaderboardEntityPrefab, leaderboardEntityHolder);
+                break;
+            case NetworkListEvent<LeaderboardEntityState>.EventType.Remove:
+                break;
+        }
+    }
+
+    public override void OnNetworkDespawn()
+    {
+        if (IsClient)
+        {
+            leaderboardEntities.OnListChanged -= HandleLeaderboardEntitiesChanged;
+        }
+
+        if (IsServer)
+        {
+            Player.OnPlayerSpawned -= HandlePlayerSpawned;
+            Player.OnPlayerDespawned -= HandlePlayerDespawned;
+        }
+    }
+
+    private void HandlePlayerSpawned(Player player)
+    {
+        leaderboardEntities.Add(new LeaderboardEntityState
+        {
+            ClientId = player.OwnerClientId,
+            PlayerName = player.PlayerName.Value
+        });
+    }
+
+    private void HandlePlayerDespawned(Player player)
+    {
+        if(leaderboardEntities == null) { return; }
+
+        foreach(LeaderboardEntityState entity in leaderboardEntities)
+        {
+            if(entity.ClientId != player.OwnerClientId) { continue; }
+
+            leaderboardEntities.Remove(entity);
+            break;
+        }
     }
 }
